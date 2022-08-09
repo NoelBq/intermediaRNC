16276d868335ea693087a5c30f54e88ea50a8634
NoelBq <noeliabaquerostaci@gmail.com>
2022-08-09 10:57:45 -0300

    removes logs

--- screens/EventsScreen.tsx
+++ screens/EventsScreen.tsx
@@ -32,7 +32,6 @@ const EventsScreen = ({ navigation }: any) => {
 
 
   const events = useSelector((state: any) => state.events)
-  console.log(events)
 
   const fetchData = async () => {
     const response = await loadEventsByOrder(10, offset * LIMIT);
